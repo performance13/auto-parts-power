367e3756249d9d0888ea10d8a1528c9f2aa812f1
Anatolii Kotenko <akotenko1313@gmail.com>
2023-01-14 00:55:36 +0200

    13.01.23

--- js/modal-improved.js
+++ js/modal-improved.js
@@ -1,6 +1,6 @@
-const popupLinks = document.querySelectorAll('.popup-link-js');
+const popupLinks = document.querySelectorAll('.popup-link');
 const body = document.querySelector('body');
-const lockPadding = document.querySelectorAll('.lock-padding');
+const lockPadding = document.querySelectorAll(".lock-padding");
 
 let unlock = true;
 
@@ -11,9 +11,37 @@ if (popupLinks.length > 0) {
     const popupLink = popupLinks[index];
     popupLink.addEventListener("click", function (e) {
       const popupName = popupLink.getAttribute('href').replace('#', '');
-      const curentPopup = document / getElementById(popupName);
+      const curentPopup = document.getElementById(popupName);
       popupOpen(curentPopup);
       e.preventDefault();
+    });
+  }
+}
+
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+if (popupCloseIcon.length > 0) {
+  for (let index = 0; index < popupCloseIcon.length; index++){
+    const el = popupCloseIcon[index];
+    el.addEventListener('click', function (e) {
+      popupClose(el.closest('.popup'));
+      e.preventDefault();
+    });
+  }
+}
+
+function popupOpen(curentPopup) {
+  if (curentPopup && unlock) {
+    const popupActive = document.querySelector('.popup.open');
+    if (popupActive) {
+      popupClose(popupActive, false);
+    } else {
+      bodyLock();
+    }
+    curentPopup.classList.add('open');
+    curentPopup.addEventListener("click", function (e) {
+      if (!e.target.closest('.popup__content')) {
+        popupClose(e.target.closest('.popup'));
+      }
     });
   }
 }
