8d1b579b813277a80dcf3a2b875dfb4a03425423
Anatolii Kotenko <akotenko1313@gmail.com>
2023-03-07 21:51:56 +0200

    08.03.23

---
+++ js/wishlist.js
@@ -0,0 +1,27 @@
+// let button = document.querySelectorAll('.wishlist-button');
+// button.addEventListener('click', () => {
+//   button.classList.toggle('active');
+// });
+
+// $(document).on('click', '.wishlist-button', function () {
+// 	var fav= $(this).hasClass('simple');
+//     if (!fav) {
+//         $(this).addClass('simple');
+//     } else {
+//         $(this).removeClass('simple');
+//     }
+// });
+
+let fav = document.querySelector('.wishlist-button');
+
+fav.addEventListener('click', () => {
+  if (fav.classList.contains('no-active')) {
+    console.log(fav.classList.contains('no-active'))
+    fav.classList.add('active')
+    fav.classList.remove('no-active')
+  } else {
+    fav.classList.add('no-active')
+    fav.classList.remove('active')
+  }
+});
+
